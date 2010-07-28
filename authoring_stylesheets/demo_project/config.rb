29cdebe140ced98653cbdfa14a6acbb74ade56cc
Chris Eppstein <chris@eppsteins.net>
2010-07-28 11:30:15 -0700

    Upload new presentation on sass 3 and compass.

---
+++ authoring_stylesheets/demo_project/config.rb
@@ -0,0 +1,11 @@
+# Require any additional compass plugins here.
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "src"
+images_dir = "images"
+javascripts_dir = "javascripts"
+
+relative_assets = true
+line_comments = false
+output_style = :expanded
