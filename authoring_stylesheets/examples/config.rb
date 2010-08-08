5e50fbfa4e39cdd6133c1183743dd1033a59537a
Chris Eppstein <chris@eppsteins.net>
2010-08-07 19:31:25 -0700

    update of the css summit talk.

---
+++ authoring_stylesheets/examples/config.rb
@@ -0,0 +1,21 @@
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "javascripts"
+fonts_dir = "fonts"
+line_comments = false
+output_style = :expanded
+
+relative_assets = true
+
+# Asset Hosts
+# relative_assets = true
+# asset_host do |asset|
+#   "http://demo-%d.csssummit.com" % (asset.hash % 4)
+# end
+# 
+# CDN (Content Distribution Network)
+# relative_assets = false
+# http_images_path = "http://cdn.css-summit.com/"
+
