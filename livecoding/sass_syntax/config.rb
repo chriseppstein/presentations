3cfaf01edc8d7a99b529a8f912b30166159ddf43
Chris Eppstein <chris@eppsteins.net>
2011-03-24 13:51:36 -0700

    tweeks

--- livecoding/sass_syntax/config.rb
+++ livecoding/sass_syntax/config.rb
@@ -9,7 +9,8 @@ javascripts_dir = "javascripts"
 output_style = :expanded
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
+
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
 
