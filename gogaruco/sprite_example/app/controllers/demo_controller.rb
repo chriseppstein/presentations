f6a250cf7f64c3c4acb2ab8703fa9fd427d1a587
Chris Eppstein <chris@eppsteins.net>
2011-09-17 09:34:00 -0700

    No sprites yet

---
+++ gogaruco/sprite_example/app/controllers/demo_controller.rb
@@ -0,0 +1,5 @@
+class DemoController < ActionController::Base
+  def index
+    render :layout => "application"
+  end
+end
---
+++ gogaruco/sprite_example/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpriteExample::Application.config.secret_token = '5fe4e15f60b87b9963affacbef4026b23f8f1913c0ce0c334e92168d9b7fbb4cd145eedba4ecddd363f1dc5893ae0d68ee6a24290c6367402c656ab5038561a1'
