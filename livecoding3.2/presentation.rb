78cff12e4fa38a217ce017960cd161c073167b25
Chris Eppstein <chris@eppsteins.net>
2012-07-20 22:53:20 -0700

    Sass updating.

--- livecoding3.2/presentation.rb
+++ livecoding3.2/presentation.rb
@@ -11,5 +11,16 @@ class Presentation < Sinatra::Application
     send_file "presentation.html"
   end
 
+  post "/compile" do
+    begin
+      options = Compass.configuration.to_sass_engine_options
+      options.update(:syntax => :scss, :style => :expanded, :line_comments => false)
+      engine = Sass::Engine.new(params[:sass], options)
+      engine.render
+    rescue Sass::SyntaxError => e
+      halt 400, e.message
+    end
+  end
+
 end
 
--- livecoding3.2/static/presentation.js
+++ livecoding3.2/static/presentation.js
@@ -1,10 +1,9 @@
 $(function() {
-	// Deck initialization
 	$.deck('.slide');
   $('.live-code').each(function(i, el) {
+    var button = $(el).find('button.compile').first();
     var sassEl = $(el).find('.sass .source').first();
     var cssEl = $(el).find('.css').first();
-    cssEl.text(sassEl.html());
 
     var htmlEl = $(el).find('.html .source').first();
     var displayEl = $(el).find('.display').first();
@@ -12,6 +11,25 @@ $(function() {
     htmlEl.keyup(function(e) {
       displayEl.html("<style>"+cssEl.text()+"</style>"+$(e.target).val());
     });
+    var updater = function() {
+      var source = sassEl.val();
+      var request = $.ajax({
+        url: "/compile",
+        type: "POST",
+        data: {sass: source},
+        dataType: "text"
+      });
+      request.done(function(result) {
+        cssEl.text(result);
+        displayEl.html("<style>"+result+"</style>"+$(htmlEl).val());
+      });
+      request.fail(function(jqXHR, textStatus) {
+        console.log(jqXHR);
+        cssEl.html('<div class="error">'+jqXHR.responseText+'</div>');
+      });
+    };
+    button.click(updater);
+    updater();
   });
 });
 
