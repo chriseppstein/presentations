e08c2f8207f8c33841e437026ffdf23a7f186443
Chris Eppstein <chris@eppsteins.net>
2013-06-28 12:56:16 -0700

    get sourcemaps stuff working.

---
+++ sass3.3/presentation.rb
@@ -0,0 +1,70 @@
+require 'sinatra'
+require 'timeout'
+
+class Presentation < Sinatra::Application
+
+  configure do
+    set(:root) {File.dirname(__FILE__)}
+    set(:public_folder) { "#{root}/static" }
+    Compass.add_project_configuration("#{File.dirname(__FILE__)}/config.rb")
+  end
+
+
+  get '/' do
+    send_file File.dirname(__FILE__)+"/presentation.html"
+  end
+
+  post "/compile" do
+    begin
+      before = Time.now
+      options = Compass.configuration.to_sass_engine_options
+      name = params[:name]
+      write_sourcemap = params[:sourcemap]
+      output_sourcemap = false
+      style = params[:style]
+      style ||= :expanded
+      if style == :sourcemap
+        output_sourcemap = true
+        style = :expanded
+      end
+      outputdir = File.dirname(__FILE__) + "/static/output"
+      sass_file = "#{outputdir}/#{name}.scss"
+      css_file  = "#{outputdir}/#{name}.css"
+      map_file  = "#{outputdir}/#{name}-map.json"
+      style = style.to_sym
+      options = options.merge(:syntax => :scss,
+                              :style => style,
+                              :line_comments => false,
+                              :filename => sass_file,
+                              :css_path => css_file,
+                              :sourcemap_path => map_file)
+      engine = Sass::Engine.new(params[:sass], options)
+      Timeout.timeout(8) do
+        css, map = if write_sourcemap || output_sourcemap
+                     engine.render_with_sourcemap("#{name}-map.json")
+                   else
+                     [engine.render, nil]
+                   end
+        map_json = map.to_json(options) if map
+        if write_sourcemap
+          FileUtils.mkdir_p(outputdir)
+          File.open(sass_file, "wb"){|f| f.write(params[:sass]) }
+          File.open(css_file, "wb"){|f| f.write(css) }
+          File.open(map_file, "wb"){|f| f.write(map_json) }
+        end
+        if output_sourcemap
+          map_json
+        else
+          css
+        end
+      end
+    rescue Sass::SyntaxError => e
+      halt 400, e.message
+    ensure
+      after ||= Time.now
+      puts "Compilation took #{after - before}"
+    end
+  end
+
+end
+
---
+++ sass3.3/static/presentation.js
@@ -0,0 +1,91 @@
+$(function() {
+	$.deck('.slide');
+  $('textarea.source,textarea.alternate-source').each(function(i,textarea) {
+    $(textarea).attr("id","source-"+i);
+  });
+  $('select.format').html("<option selected>expanded</option><option>compact</option><option>compressed</option><option>nested</option><option>sourcemap</option>");
+  $('.live-code section:not(.only) > :first-child').before("<div class='resizer'>⇿</div>");
+  $('.live-code section.only > :first-child').before("<div class='resizer'>⇳</div>");
+  // Create the full-size togglers
+  $('.live-code, .pretend-code').find('.resizer').click(function(e) {
+    $(e.target).parent("section").toggleClass("fullsize");
+  });
+
+  $('.notes').each(function(i, el) {
+    var notectrl = $('<a data-note-control>&nbsp;&nbsp;<span data-show>Notes</span><span data-hide>Hide</span></a>');
+    notectrl.click(function() { $(el).toggleClass("visible"); });
+    $(el).append(notectrl);
+  });
+
+  $('.live-code').each(function(i, el) {
+    var button = $(el).find('button.compile').first();
+    var format = $(el).find('.css').first().attr("data-output-format");
+    if (format) { $(el).find("select.format").val(format); }
+    $(el).find("select.format").after("<label class='sourcemap'><input type='checkbox' name='sourcemap' value='sourcemap'>Sourcemap</label>");
+    var cssEl = $(el).find('.css .output').first();
+
+    // Create the source changers
+    $(el).find(".source").parent("section").each(function(i,section) {
+      var textareas = $(section).find("textarea");
+      if (textareas.size() > 1) {
+        var options = textareas.map(function(i, ta) {
+          ta = $(ta);
+          return "<option value='"+ta.attr("id")+"'>"+ta.attr("title")+"</option>";
+        }).toArray().join();
+        $(section).find("textarea").last().after("<label class='source-changer'>Change code to: </label>").after("<select class='source-changer'>"+options+"</select>");
+      }
+    });
+    $("select.source-changer").change(function(e) {
+      $(e.target).parent("section").find("textarea").removeClass("source").addClass("alternate-source");
+      $("#"+$(e.target).val()).addClass("source");
+      updater();
+    });
+
+    // convenience method for finding the html source
+    var htmlEl = function() {
+      return $(el).find('.html .source').first();
+    }
+    var displayEl = $(el).find('.result .output').first();
+    // Set up the initial html result
+    displayEl.html("<style>"+cssEl.text()+"</style>"+htmlEl().text());
+    // Monitor the the html for changes and update the results
+    htmlEl().keyup(function(e) {
+      displayEl.html("<style>"+cssEl.text()+"</style>"+$(e.target).val());
+    });
+    // The updater compiles sass and inserts the resulting css into the page.
+    var updater = function() {
+      var sourceEl = $(el).find('.sass .source').first()
+      var source = sourceEl.val();
+      var id = sourceEl.attr("id");
+      var sourcemap = $(el).find("label.sourcemap input:checked").val();
+      var request = $.ajax({
+        url: "/compile",
+        type: "POST",
+        data: {sass: source, style: $(el).find("select.format").val(), name: id, sourcemap: sourcemap},
+        dataType: "text"
+      });
+      request.done(function(result) {
+        cssEl.text(result);
+        cssEl.parent("section").find(".filesize").html(result.length + " bytes");
+        if (sourcemap) {
+          var style = "<link rel='stylesheet' href='/output/"+id+".css'>";
+        } else {
+          var style = "<style>"+result+"</style>";
+        }
+        displayEl.html(style+$(htmlEl()).val());
+      });
+      // It happens, ok?
+      request.fail(function(jqXHR, textStatus) {
+        console.log(jqXHR);
+        cssEl.html('<div class="error">'+jqXHR.responseText+'</div>');
+      });
+    };
+    // Update sass when the button is clicked, when the format changes, and at the start.
+    button.click(updater);
+    $(el).live('.sass .source').keyup(updater);
+    $(el).find("select.format").change(updater);
+    $(el).find("label.sourcemap input").change(updater);
+    updater();
+  });
+});
+
